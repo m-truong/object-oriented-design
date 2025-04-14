53601a4b9afb29d9e06cf13a1c85628e8027e3a0
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-14 17:26:21 -0400

    Fix formatting in app.listen comment for consistency

--- memory-server.js
+++ memory-server.js
@@ -65,7 +65,7 @@ app.get('/disk/:key', (req, res) => {
 
 // this event-listener will trigger the an open-socket
 //  while running this file, whatever hardware-machine
-//  that i'm running this file on will be accepting 
+//  that i'm running this file on will be accepting
 //  HTTP - packet - requests from other client-hardware-machines
 app.listen(1991, () => {
     console.log(`one long river of song on 1991`);
